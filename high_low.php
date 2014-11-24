ab7a4e85316d166f705f631c4891cf7156832058
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-24 11:42:31 -0600

    adds numeric check to high low game

--- high_low.php
+++ high_low.php
@@ -44,21 +44,56 @@
 
 
 
+// $ranNum = mt_rand(1, 100);
+// $numGuess = 0;
+
+// fwrite(STDOUT, "Our random number is {$ranNum}\n");
+
+// do {
+// 	fwrite(STDOUT, "Guess? ");
+// 	$guess = fgets(STDIN);
+// 	$numGuess++;
+
+// 	if ($guess > $ranNum) {
+// 		fwrite(STDOUT, "LOWER \n");
+// 	} elseif ($guess < $ranNum) {
+// 		fwrite(STDOUT, "HIGHER \n");
+// 	}
+
+// } while ($guess != $ranNum);
+
+// fwrite(STDOUT, "Good Guess!!\n");
+// fwrite(STDOUT, "Number of guesses: {$numGuess}\n");
+
+
+// exit(0);
+
+
+
 $ranNum = mt_rand(1, 100);
 $numGuess = 0;
 
 fwrite(STDOUT, "Our random number is {$ranNum}\n");
 
 do {
-	fwrite(STDOUT, "Guess? ");
-	$guess = fgets(STDIN);
+	fwrite(STDOUT, "Please guess a number: ");
+	$guess = trim(fgets(STDIN));
 	$numGuess++;
 
-	if ($guess > $ranNum) {
-		fwrite(STDOUT, "LOWER \n");
-	} elseif ($guess < $ranNum) {
-		fwrite(STDOUT, "HIGHER \n");
+	if (is_numeric($guess)) {
+
+		if ($guess > $ranNum) {
+			fwrite(STDOUT, "LOWER \n");
+		} 
+
+		elseif ($guess < $ranNum) {
+			fwrite(STDOUT, "HIGHER \n");
+		}
 	}
+	else {
+		fwrite(STDOUT, "I said numeric. Try again.\n");
+		}
+		
 
 } while ($guess != $ranNum);
 
@@ -67,3 +102,6 @@ fwrite(STDOUT, "Number of guesses: {$numGuess}\n");
 
 
 exit(0);
+
+
+

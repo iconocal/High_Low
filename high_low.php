90686c552cc764b64cf86fc9304f4414e5de7a19
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-24 10:32:45 -0600

    high low game using mt_rand

--- high_low.php
+++ high_low.php
@@ -1,7 +1,28 @@
 <?php
 
 
-$ranNum = rand(1, 100);
+// $ranNum = rand(1, 100);
+
+// fwrite(STDOUT, "Our random number is {$ranNum}\n");
+
+// do {
+// 	fwrite(STDOUT, "Guess? ");
+// 	$guess = fgets(STDIN);
+
+// 	if ($guess > $ranNum) {
+// 		fwrite(STDOUT, "LOWER \n");
+// 	} elseif ($guess < $ranNum) {
+// 		fwrite(STDOUT, "HIGHER \n");
+// 	}
+
+// } while ($guess != $ranNum);
+
+// fwrite(STDOUT, "Good Guess!!\n");
+
+// exit(0);
+
+
+$ranNum = mt_rand(1, 100);
 
 fwrite(STDOUT, "Our random number is {$ranNum}\n");
 
@@ -20,7 +41,6 @@ do {
 fwrite(STDOUT, "Good Guess!!\n");
 
 exit(0);
-
 
 
 

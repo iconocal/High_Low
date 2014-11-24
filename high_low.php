60072c91c5f833b90df8fb7e00586fa8b2319609
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-24 10:40:05 -0600

    high low game with added guess counter

--- high_low.php
+++ high_low.php
@@ -22,13 +22,37 @@
 // exit(0);
 
 
+// $ranNum = mt_rand(1, 100);
+
+// fwrite(STDOUT, "Our random number is {$ranNum}\n");
+
+// do {
+// 	fwrite(STDOUT, "Guess? ");
+// 	$guess = fgets(STDIN);
+
+// 	if ($guess > $ranNum) {
+// 		fwrite(STDOUT, "LOWER \n");
+// 	} elseif ($guess < $ranNum) {
+// 		fwrite(STDOUT, "HIGHER \n");
+// 	}
+
+// } while ($guess != $ranNum);
+
+// fwrite(STDOUT, "Good Guess!!\n");
+
+// exit(0);
+
+
+
 $ranNum = mt_rand(1, 100);
+$numGuess = 0;
 
 fwrite(STDOUT, "Our random number is {$ranNum}\n");
 
 do {
 	fwrite(STDOUT, "Guess? ");
 	$guess = fgets(STDIN);
+	$numGuess++;
 
 	if ($guess > $ranNum) {
 		fwrite(STDOUT, "LOWER \n");
@@ -39,8 +63,7 @@ do {
 } while ($guess != $ranNum);
 
 fwrite(STDOUT, "Good Guess!!\n");
+fwrite(STDOUT, "Number of guesses: {$numGuess}\n");
+
 
 exit(0);
-
-
-
